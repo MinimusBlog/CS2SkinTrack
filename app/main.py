523281ad81bf80204632d4aaae216168cb96fa56
Liza <puzanovaliza6666@mail.ru>
2025-04-11 14:34:09 +0300

    fix prblms  merge

--- app/main.py
+++ app/main.py
@@ -1,21 +1,18 @@
-import os,logging,telebot
+import telebot
 from dotenv import load_dotenv
-<<<<<<< HEAD
 import os
-=======
->>>>>>> d10df0ad0296d1a73ddc785fd37664ab074aeb22
 from flask import Flask, request
 from handlers import register_handlers
 
 # Загрузка переменных окружения из .env файла
 load_dotenv()
+bot_token = os.getenv('TELEBOT_TOKEN')
 
 app = Flask(__name__)
-bot = telebot.TeleBot(os.getenv("TELEBOT_TOKEN"))
+bot = telebot.TeleBot(bot_token)
 
 register_handlers(bot) # Регистрация обработчиков
 
-bot.infinity_polling()
 # Установка вебхука
 @app.route('/' + bot_token, methods=['POST'])
 def getMessage():
@@ -26,13 +23,8 @@ def getMessage():
 @app.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://3df4-183-96-15-159.ngrok-free.app/" + bot_token)
+    bot.set_webhook(url="https://5d4f-14-33-225-165.ngrok-free.app/" + bot_token)
     return "!", 200
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     app.run(host="localhost", port=5000) # Слушаем 5000 порт
-=======
-    app.run(host="localhost", port=5000)  # Запуск на порту 5000
-bot.infinity_polling()
->>>>>>> d10df0ad0296d1a73ddc785fd37664ab074aeb22
